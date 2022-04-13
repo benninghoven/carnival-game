7278b8d3d784f0bebbd8a082dfcb94eb167f3683
benninghoven <devinbenninghoven@gmail.com>
2022-04-12 19:21:34 -0700

    added python files

---
+++ src/plans.py
@@ -0,0 +1,34 @@
+print("this shit works properly")
+
+
+"""
+init
+play carnival music
+rgbstrip lights startup animation
+
+gui PLAY button 
+    LEADERBOARD
+game logic
+
+tell you stats 
+leaderbeard
+
+store leaderboard in a .whatever
+
+init.()
+mario cart race start mp3
+
+while GAME:
+    GUI - Play - GAMEMODE? 
+        - Leaderboard
+
+// FILE // DIFFICULTY
+play.py *****
+init.py ****
+leaderboard.py *
+
+gui.py *****
+
+
+
+"""
