33087026b6bb4e9ad330e9d634dd00f8dc218f76
benninghoven <benninghoven@gmail.com>
2022-04-18 19:14:01 -0700

    added servo.py module

---
+++ src/servo.py
@@ -0,0 +1,16 @@
+from adafruit_servokit import ServoKit
+import time
+
+kit = ServoKit(channels=8)
+servo = kit.servo[1]
+
+SLEEP = .1
+
+def UnmuteMic():
+    servo.angle = 0
+    time.sleep(SLEEP)
+    servo.angle = 10
+    time.sleep(SLEEP)
+    servo.angle = 0
+
+UnmuteMic()
